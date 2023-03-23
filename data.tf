9bc927391ff7644cf5ea128d8ff88fc368ae9fca
Nelahio <57316380+Nelahio@users.noreply.github.com>
2023-03-23 14:15:40 +0100

    add data and terraform

---
+++ data.tf
@@ -0,0 +1,18 @@
+data "azurerm_resource_group" "rg-maalsi" {
+  name = "rg-${var.project_name}${var.environment_suffix}"
+}
+
+data "azurerm_key_vault" "kv" {
+  name = "kv-${var.project_name}${var.environment_suffix}"
+  resource_group_name = data.azurerm_resource_group.rg-maalsi.name
+}
+
+data "azurerm_key_vault_secret" "postgres-login" {
+  name="postgres-login"
+  key_vault_id = data.azurerm_key_vault.kv.id
+}
+
+data "azurerm_key_vault_secret" "postgres-password" {
+  name="postgres-password"
+  key_vault_id = data.azurerm_key_vault.kv.id
+}
